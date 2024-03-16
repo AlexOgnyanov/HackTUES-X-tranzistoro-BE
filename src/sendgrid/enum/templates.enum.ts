2bd5112a703339798fcf0db9b5738763d225601a
AlexOgnyanov <133657810+AlexOgnyanov@users.noreply.github.com>
2024-03-16 06:04:35 +0200

    Added .env.example (#3)

--- src/sendgrid/enum/templates.enum.ts
+++ src/sendgrid/enum/templates.enum.ts
@@ -1,5 +1,5 @@
 export enum Templates {
-  EmailVerification = 'd-966e6bcbc4724b7f938d7975974b5cec',
+  EmailVerification = 'd-36f8e86b32b249b2ac9401e56b899bb3',
   PasswordReset = 'd-8834c880f8aa41658a3be23ec9201842',
   PasswordChange = 'd-3fbcd1a05a824c1b990082667c6bc109',
 }
